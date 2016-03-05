6e53e9a9b1888236c2f9b1b5688a47020ade0a9b
dharada <dharada6@gmail.com>
2016-03-05 17:05:29 +0900

    for saving changes

--- src/main/java/search/BinarySearchTree.java
+++ src/main/java/search/BinarySearchTree.java
@@ -13,6 +13,40 @@ public class BinarySearchTree {
     // 木の深さ
     private int depth = 0;
 
+    public boolean find(Node node, int target) {
+
+        if (node == null) {
+            return false;
+        }
+
+        if (node.data > target) {
+            //小さい値なので、右側にはない
+            return find(node.left, target);
+        } else if (node.data < target) {
+            // 大きい値なので、左側にはない
+            return find(node.right, target);
+        }
+
+        return true;
+    }
+
+    public void displayAll(Node node) {
+
+        if (node == null) {
+            // end
+            return;
+        }
+
+        System.out.println(node.data);
+
+        if (node.left != null) {
+            displayAll(node.left);
+        } else if (node.right != null) {
+            displayAll(node.right);
+        }
+
+    }
+
     public void insertNode(int newData, Node node) {
 
         // nodeが一つもない場合
@@ -35,9 +69,9 @@ public class BinarySearchTree {
                 // 左に追加
                 node.left = new Node(newData);
 
-                //                if (node.right == null) {
-                //                    depth++;
-                //                }
+                if (node.right == null) {
+                    depth++;
+                }
             }
 
         } else if (node.data < newData) {
@@ -47,12 +81,13 @@ public class BinarySearchTree {
             if (node.right != null) {
                 insertNode(newData, node.right);
             } else {
+
                 // 右へ追加
                 node.right = new Node(newData);
 
-                //                if (node.left == null) {
-                //                    depth++;
-                //                }
+                if (node.left == null) {
+                    depth++;
+                }
 
             }
         }
