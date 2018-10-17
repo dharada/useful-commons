28d4ff6d60f5ec264d171ad3a0550ea802af7b6c
dharada <dharada6@gmail.com>
2018-10-17 18:25:57 +0900

    for a case Synonym token filter

--- src/test/java/parse/JsonReplaceTest.java
+++ src/test/java/parse/JsonReplaceTest.java
@@ -13,9 +13,5 @@ public class JsonReplaceTest {
 //        new JsonReplace().execute("C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/400.json", "C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/out-data.json");
         new JsonReplace().execute("C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/data-utf8.json", "C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/out-data-long.json");
         new JsonReplace().replaceHhtmlsourceField("C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/out-data-long.json", "C:\\Elastic\\app\\6.3.1\\elasticsearch-6.3.1\\json_new/jq-data.json");
-
-
-        // remember, I don't know why but I needed to add "}" in the end of jq-data.json file. so I added by hand...
-
     }
 }
