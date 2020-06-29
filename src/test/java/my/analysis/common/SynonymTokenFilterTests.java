a2a24eb8f432953d4700cecb5f659f8956a41e47
Daisuke Harada <dharada6@gmail.com>
2020-06-29 17:05:30 +0900

    refactoring

--- src/test/java/my/analysis/common/SynonymTokenFilterTests.java
+++ src/test/java/my/analysis/common/SynonymTokenFilterTests.java
@@ -2,18 +2,14 @@ package my.analysis.common;
 
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
-import org.apache.lucene.analysis.ja.JapaneseNumberFilterFactory;
 import org.apache.lucene.analysis.ja.JapaneseTokenizerFactory;
 import org.apache.lucene.analysis.synonym.SynonymFilterFactory;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.analysis.util.ClasspathResourceLoader;
-import org.apache.lucene.analysis.util.FilesystemResourceLoader;
-import org.apache.lucene.analysis.util.ResourceLoader;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,27 +18,15 @@ public class SynonymTokenFilterTests {
     @Test
     public void testSynonymTokenFilter() throws IOException {
 
-        Map<String, String> settigMap = new HashMap<>();
-        String userDictionaryKey = "userDictionary";
-        settigMap.put(userDictionaryKey, "vos-ja_user-dict_yh_tmp.txt");
+        JapaneseTokenizerFactory japaneseTokenizerFactory = getJapaneseTokenizerFactory();
 
-        JapaneseTokenizerFactory japaneseTokenizerFactory = new JapaneseTokenizerFactory(settigMap);
-        Tokenizer tokenizer = japaneseTokenizerFactory.create();
+        TokenStream tokenStream = getTokenStreamForSynonymTokenFilter(japaneseTokenizerFactory);
 
-        String myInput = "スーピマコットンストレッチシャツ";
-        tokenizer.setReader(new StringReader(myInput));
-
-        Map<String, String> synonymsSettingMap = new HashMap<>();
-        synonymsSettingMap.put("synonyms", "vos-ja_contract-synonym_yh_tmp.txt");
-
-        SynonymFilterFactory synonymFilterFactory = new SynonymFilterFactory(synonymsSettingMap);
-
-        ResourceLoader loader = new ClasspathResourceLoader();
-        synonymFilterFactory.inform(loader);
-
-        TokenStream tokenStream = synonymFilterFactory.create(tokenizer);
+//        LowerCaseFilterFactory lowerCaseFilterFactory = new LowerCaseFilterFactory(new HashMap<>());
+//        TokenStream tokenStream = lowerCaseFilterFactory.create(tokenizer);
 
         OffsetAttribute offsetAtt = tokenStream.addAttribute(OffsetAttribute.class);
+
         try {
             tokenStream.reset(); // Resets this stream to the beginning. (Required)
 
@@ -61,6 +45,32 @@ public class SynonymTokenFilterTests {
             tokenStream.close(); // Release resources associated with this stream.
         }
 
+    }
+
+    private JapaneseTokenizerFactory getJapaneseTokenizerFactory() throws IOException {
+
+        Map<String, String> settingsMapForTokenizer = new HashMap<>();
+        String userDictionaryKey = "userDictionary";
+        settingsMapForTokenizer.put(userDictionaryKey, "vos-ja_user-dict_yh_tmp.txt");
+
+        JapaneseTokenizerFactory japaneseTokenizerFactory = new JapaneseTokenizerFactory(settingsMapForTokenizer);
+        japaneseTokenizerFactory.inform(new ClasspathResourceLoader());
+
+        return japaneseTokenizerFactory;
+    }
+
+    private TokenStream getTokenStreamForSynonymTokenFilter(JapaneseTokenizerFactory japaneseTokenizerFactory) throws IOException {
+
+        Tokenizer tokenizer = japaneseTokenizerFactory.create();
+        String myInput = "スーピマコットンストレッチシャツ";
+        tokenizer.setReader(new StringReader(myInput));
+
+        Map<String, String> synonymsSettingMap = new HashMap<>();
+        synonymsSettingMap.put("synonyms", "vos-ja_contract-synonym_yh_tmp.txt");
+        SynonymFilterFactory synonymFilterFactory = new SynonymFilterFactory(synonymsSettingMap);
+        synonymFilterFactory.inform(new ClasspathResourceLoader());
+
+        return synonymFilterFactory.create(tokenizer);
     }
 
 }
