44c1cf9c9ed37585715facaf3900b0b0fdc5e3bd
dharada <dharada6@gmail.com>
2023-04-09 20:08:33 +0900

    sql loader related left outer join OK!

--- src/main/java/jdbc/SftpConnectionTest.java
+++ src/main/java/jdbc/SftpConnectionTest.java
@@ -18,27 +18,36 @@ public class SftpConnectionTest {
 
             String jdbcUrl = "jdbc:csv:/sftp://52.140.192.205:22?user=daisuke&password=Iccpf12171217";
 
-            Properties props =new Properties();
-            props.put("_CSV_Header","true");
+            Properties props = new Properties();
+            props.put("_CSV_Header", "true");
             props.put("_CSV_Separator", ",");
 
             // Connect to the SFTP server
-            Connection conn = DriverManager.getConnection(jdbcUrl,props);
+            Connection conn = DriverManager.getConnection(jdbcUrl, props);
 
 
             // Test the connection by querying a file on the server
-            String sql = "SELECT e.empId as empId, e.fullName as fullName, e.email, e.contactNo, d.deptName as deptName, d.role as role \n" +
-                    "FROM Employee e \n " +
-                    "LEFT OUTER JOIN Dept d ON e.deptId = d.id ";
+            String sql = "SELECT e.empId as empId, e.fullName as fullName, e.email, e.contactNo, d.deptName as deptName, d.role as role, CONCAT('\"', GROUP_CONCAT(gs.groupName SEPARATOR ','), '\"') AS groupName \n" +
+                    "FROM Employee e LEFT OUTER JOIN Dept d ON e.deptId = d.id INNER JOIN EmployeeGroups eg ON e.empId = eg.empId INNER JOIN Groups gs ON eg.groupId = gs.groupId GROUP BY empId";
 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
 
-            while(rs.next()) {
-                for(int i=1; i<=rs.getMetaData().getColumnCount(); i++) {
+            int rowIndex = 0;
+            while (rs.next()) {
+
+                if (rowIndex == 0) {
+                    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                        System.out.print(rs.getMetaData().getColumnName(i) + "\t");
+                    }
+                    System.out.println();
+                }
+
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     System.out.print(rs.getString(i) + "\t");
                 }
                 System.out.println();
+                rowIndex++;
             }
 
             // Close the connection
