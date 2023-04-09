20c98c304944a11afd4fc995ee44ed1c70259d07
daisuke-harada-sp <daisuke.harada@sailpoint.com>
2023-04-09 21:39:09 +0900

    test date convertion on Watcher elastic

--- src/main/java/jdbc/SftpConnectionTest.java
+++ src/main/java/jdbc/SftpConnectionTest.java
@@ -15,12 +15,18 @@ public class SftpConnectionTest {
             // Define the JDBC URL for the SFTP server
             //String jdbcUrl = "jdbc:csv:sftp://20.222.143.73/home/daisuke?user=daisuke&password=Iccpf12171217";
 
-            String jdbcUrl = "jdbc:csv:/sftp://20.222.143.73:22/home/daisuke?user=daisuke&password=Iccpf12171217";
+            String jdbcUrl = "jdbc:csv:sftp://20.89.134.84?user=daisuke&password=Iccpf12171217";
             // Connect to the SFTP server
             Connection conn = DriverManager.getConnection(jdbcUrl);
 
+            System.out.println(conn.getMetaData().getDatabaseProductName());
+            System.out.println(conn.getMetaData().getDriverName());
+
+
+
+
             // Test the connection by querying a file on the server
-            String sql = "SELECT * FROM Employee.csv";
+            String sql = "SELECT * FROM Employee";
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
