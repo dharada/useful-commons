fdea8fe2861eeca716972508c64035997777dab7
daisuke-harada-sp <daisuke.harada@sailpoint.com>
2023-04-16 23:30:27 +0900

    for now..

--- src/main/java/jdbc/SftpConnectionTest.java
+++ src/main/java/jdbc/SftpConnectionTest.java
@@ -1,5 +1,8 @@
 package jdbc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,15 +11,15 @@ import java.util.Properties;
 
 public class SftpConnectionTest {
 
+
     public static void main(String[] args) {
         try {
             // Load the HXTT Text JDBC driver
             Class.forName("com.hxtt.sql.text.TextDriver");
 
             // Define the JDBC URL for the SFTP server
-            //String jdbcUrl = "jdbc:csv:sftp://20.222.143.73/home/daisuke?user=daisuke&password=Iccpf12171217";
-
-            String jdbcUrl = "jdbc:csv:/sftp://52.140.192.205:22?user=daisuke&password=Iccpf12171217";
+            String jdbcUrl = "jdbc:csv:/sftp://52.140.192.205:22/sqlloader?user=daisuke&password=" +
+                    getPassword();
 
             Properties props = new Properties();
             props.put("_CSV_Header", "true");
@@ -27,11 +30,15 @@ public class SftpConnectionTest {
 
 
             // Test the connection by querying a file on the server
-            String sql = "SELECT e.empId as empId, e.fullName as fullName, e.email, e.contactNo, d.deptName as deptName, d.role as role, gs.groupName as groupName \n" +
-                    "FROM Employee e " +
-                    "LEFT OUTER JOIN Dept d ON e.deptId = d.id \n" +
-                    "INNER JOIN EmployeeGroups eg ON e.empId = eg.empId \n" +
-                    "INNER JOIN Groups gs ON eg.groupId = gs.groupId";
+            String sql =
+                    "SELECT e.empId as empId, e.fullName as fullName, e.email, e.contactNo, d.deptName as deptName, d.role as role\n" +
+                            "                    FROM Employee e \n" +
+                            "                    LEFT OUTER JOIN EmpDept d ON e.empId = d.empId";
+
+//            sql = "select roles.role as role, roles.description as description from Roles roles INNER JOIN EmpDept d ON roles.role = d.role group by roles.role";
+
+            //     sql  = "select r.role as role, r.description as description from Roles r";
+
 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
@@ -60,6 +67,22 @@ public class SftpConnectionTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getPassword() {
+
+
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = SftpConnectionTest.class.getResourceAsStream("/sftp-connection-config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Retrieve properties
+        return properties.getProperty("password");
+
     }
 
 }
