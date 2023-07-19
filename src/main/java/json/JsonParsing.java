69b2979e8a16b7f188894e69c9019cf857e60f6d
dharada <dharada6@gmail.com>
2023-07-19 09:10:02 +0900

    OpenText BeforeModifyAccount

--- src/main/java/json/JsonParsing.java
+++ src/main/java/json/JsonParsing.java
@@ -6,6 +6,7 @@ import org.json.simple.JSONObject;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.List;
 
 public class JsonParsing {
 
@@ -26,15 +27,25 @@ public class JsonParsing {
 //    System.out.println(startsWith); // Output: true
 
     new JsonParsing().parse("");
-
   }
 
   public void parse(String text) throws IOException {
-    Path file = Paths.get("/Users/daisuke.harada/github/dharada/jdbc-provision-quickstart/connector-rule/BlackLine-BeforeApproveRule.txt");
-    String fileStringWithLF = Files.readString(file);
-    checkIllegalCharacters(fileStringWithLF, file);
-    String fileStrWithCRLF = fileStringWithLF.replaceAll("\n", "\r\n");
+//    Path file = Paths.get("/Users/daisuke.harada/github/dharada/jdbc-provision-quickstart/connector-rule/BlackLine-BeforeApproveRule.txt");
+    Path file = Paths.get("/Users/daisuke.harada/github/dharada/jdbc-provision-quickstart/connector-rule/OpenText-WSBO.txt");
+
+    //  Path file = Paths.get("/Users/daisuke.harada/github/dharada/jdbc-provision-quickstart/connector-rule/SQLLoader-Provision-Rule-sample.txt");
+
+    List<String> fileStringWithLF = Files.readAllLines(file);
+    for (int i = 0; i < fileStringWithLF.size(); i++) {
+      checkIllegalCharacters(eachLine(fileStringWithLF, i), file);
+    }
+
+    String fileStrWithCRLF = Files.readString(file).replaceAll("\n", "\r\n");
     System.out.println(JSONObject.toString("script", fileStrWithCRLF));
+  }
+
+  private String eachLine(List<String> fileStringWithLF, int i) {
+    return fileStringWithLF.get(i);
   }
 
   private static void checkIllegalCharacters(String fileStringWithLF, Path file) throws IOException {
@@ -42,8 +53,9 @@ public class JsonParsing {
       throw new RuntimeException("static is included in the connector-rule impl.(file path=" + file.toAbsolutePath() + ")" + fileStringWithLF);
     }
 
-    if (fileStringWithLF.contains("<A") || fileStringWithLF.contains("<S") || fileStringWithLF.contains("<M") || fileStringWithLF.contains("<I") || fileStringWithLF.contains(">")) {
+    if (fileStringWithLF.contains("<A") || fileStringWithLF.contains("<S") || fileStringWithLF.contains("<M") || fileStringWithLF.contains("<I") || (fileStringWithLF.contains(">") && !fileStringWithLF.contains(" > 0"))) {
       throw new RuntimeException("< or > is included in the connector-rule impl.(file path=" + file.toAbsolutePath() + ")" + fileStringWithLF);
     }
+
   }
 }
