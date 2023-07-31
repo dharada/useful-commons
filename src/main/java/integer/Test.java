e01686104ccd784b0981ec4a9f21221237e28d05
dharada <dharada6@gmail.com>
2023-08-01 00:23:02 +0900

    Integer null check..

---
+++ src/main/java/integer/Test.java
@@ -0,0 +1,16 @@
+package integer;
+
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println(new Test().hoge());
+  }
+
+  private Integer hoge() {
+    Integer hoge = null;
+    if (hoge == null) {
+      System.out.println("hoge == null");
+    }
+    return hoge;
+  }
+}
