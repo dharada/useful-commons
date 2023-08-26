103b7c749a49ed5dffe5378be5506475a880345f
dharada <dharada6@gmail.com>
2023-08-26 11:51:56 +0900

    unnecessary commit..

--- src/main/java/collect/Lists.java
+++ src/main/java/collect/Lists.java
@@ -1,5 +1,6 @@
 package collect;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,9 +8,47 @@ import java.util.List;
  */
 public class Lists {
 
-    public static <E> List<E> newArrayList(E... elements) {
-        return com.google.common.collect.Lists.newArrayList(elements);
+  public static <E> List<E> newArrayList(E... elements) {
+    return com.google.common.collect.Lists.newArrayList(elements);
+  }
+
+
+  public static void main(String[] args) {
+    List hoge = new ArrayList<>();
+    hoge.add("01");
+    hoge.add("02");
+
+    System.out.println(hoge.toString());
+
+    List attempt = new ArrayList<>();
+    new Lists().setList(hoge, attempt);
+
+    System.out.println(attempt.size());
+    System.out.println(attempt.toString());
+
+  }
+
+
+  private void setList(Object entitlementValueObject, List<String> list) {
+    if (isNotNull(entitlementValueObject)) {
+      if (entitlementValueObject instanceof String) {
+        list.add((String) entitlementValueObject);
+      } else if (entitlementValueObject instanceof List) {
+        List<String> elist = (List<String>) entitlementValueObject;
+        for (String entVal : elist) {
+          list.add(entVal);
+        }
+      }
     }
+  }
+
+  private boolean isNotNull(Object obj) {
+    return !isNull(obj);
+  }
+
+  private boolean isNull(Object obj) {
+    return obj == null;
+  }
 
 }
 
